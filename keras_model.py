d2eafb35dab7285ca06e0d2b9e78a3046f986ba9
GraC2H5OH <82993704+GraC2H5OH@users.noreply.github.com>
2021-08-24 20:18:30 +0300

    Add files via upload

---
+++ keras_model.py
@@ -0,0 +1,9 @@
+import pytesseract
+import cv2
+
+def model(file):
+    pytesseract.pytesseract.tesseract_cmd = 'D:\\Tesseract-OCR\\tesseract.exe'
+    img = cv2.imread(file)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    text=pytesseract.image_to_string(img)
+    return text[:-1]
